cea6b0bd20b943f051a0a57780de1ac26db9d49d
Déryck Freitas <deryck.ferreira30@gmail.com>
2023-04-24 22:22:34 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_AdotAki/Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_AdotAki.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        public IActionResult Confirmar()
+        {
+            return View();
+        }
+    }
+}
